6f39d930d0cf22ad8456074d6cd449f0ac380d69
2nthony <ant.hu@outlook.com>
2022-05-17 18:37:31 +0800

    refactor(nvim): treesitter remove useless

--- .config/nvim/after/plugin/nvim-treesitter.lua
+++ .config/nvim/after/plugin/nvim-treesitter.lua
@@ -27,5 +27,3 @@ configs.setup {
   },
 }
 
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
