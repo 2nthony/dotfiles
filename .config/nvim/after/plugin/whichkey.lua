badc4a2018e0ac2cc168c79db98ec56c3bfb67f7
2nthony <ant.hu@outlook.com>
2022-05-21 15:10:10 +0800

    feat(nvim): which key winblend

--- .config/nvim/after/plugin/whichkey.lua
+++ .config/nvim/after/plugin/whichkey.lua
@@ -47,7 +47,7 @@ local setup = {
     position = "bottom", -- bottom, top
     margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
     padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-    winblend = 0,
+    winblend = 5,
   },
   layout = {
     height = { min = 4, max = 25 }, -- min and max height of the columns
