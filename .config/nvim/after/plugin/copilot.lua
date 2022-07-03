2d4ab5a1d692035932c4dea4627658673c2edabd
2nthony <ant.hu@outlook.com>
2022-07-03 17:28:04 +0800

    fix(nvim): enable github copilot

--- .config/nvim/after/plugin/copilot.lua
+++ .config/nvim/after/plugin/copilot.lua
@@ -6,9 +6,14 @@ if not status_copilot_ok then
 end
 
 if vim.g.loaded_copilot then
+  -- vim.cmd([[
+  --   autocmd BufEnter * let b:copilot_enabled = v:false
+  -- ]])
+
   vim.cmd([[
-    autocmd BufEnter * let b:copilot_enabled = v:false
+    imap <silent><script><expr> <s-tab> copilot#Accept("\<CR>")
+    autocmd BufEnter * let g:copilot_no_tab_map = v:true
   ]])
 end
 
-copilot.setup{}
+-- copilot.setup{}
