33ba2879f39e0e6fcbe0fa98abbaf58fcdf59caf
2nthony <ant.hu@outlook.com>
2022-07-04 14:18:25 +0800

    fix(nvim): lsp diagnostic can not focusable

--- .config/nvim/after/plugin/lspconfig.lua
+++ .config/nvim/after/plugin/lspconfig.lua
@@ -201,7 +201,9 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
   border = "rounded",
+  focusable = false,
 })
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
   border = "rounded",
+  focusable = false,
 })
