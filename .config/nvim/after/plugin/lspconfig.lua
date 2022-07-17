3f4324fa0515e921e4322e773bd754509b93c9e1
2nthony <ant.hu@outlook.com>
2022-07-17 20:35:10 +0800

    feat(nvim): lua lsp

--- .config/nvim/after/plugin/lspconfig.lua
+++ .config/nvim/after/plugin/lspconfig.lua
@@ -33,6 +33,30 @@ nvim_lsp.volar.setup {
   on_attach = on_attach,
   capabilities = capabilities
 }
+nvim_lsp.sumneko_lua.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim', 'use'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 
 nvim_lsp.jsonls.setup {
   on_attach = on_attach,
