b61a23eaef75b572f16585a7cbdfe99a9b81cb1a
2nthony <ant.hu@outlook.com>
2022-07-17 20:35:23 +0800

    refactor(nvim): remove default config

--- .config/nvim/after/plugin/rust-tools.lua
+++ .config/nvim/after/plugin/rust-tools.lua
@@ -8,13 +8,11 @@ local capabilities = require'lsp.capabilities'
 
 rust_tools.setup{
   tools = {
-    autoSetHints = true,
-      hover_with_actions = true,
-      inlay_hints = {
-        show_parameter_hints = false,
-        parameter_hints_prefix = "",
-        other_hints_prefix = "",
-      },
+    inlay_hints = {
+      show_parameter_hints = false,
+      parameter_hints_prefix = "",
+      other_hints_prefix = "",
+    },
   },
 
   server = {
