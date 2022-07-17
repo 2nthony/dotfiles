577c877436236402ae88d6f738f8a55b494df6c4
2nthony <ant.hu@outlook.com>
2022-07-17 13:14:20 +0800

    refactor(nvim): move lsp on_attach, capabilities

---
+++ .config/nvim/lua/lsp/capabilities.lua
@@ -0,0 +1,6 @@
+-- Set up completion using nvim_cmp with LSP source
+local capabilities = require('cmp_nvim_lsp').update_capabilities(
+  vim.lsp.protocol.make_client_capabilities()
+)
+
+return capabilities
