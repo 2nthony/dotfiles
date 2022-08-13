fdf9e67818e6c1a25fc2dfb4d95c3fa904d89c6b
2nthony <ant.hu@outlook.com>
2022-08-13 15:07:53 +0800

    clean telescope media files config

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -124,10 +124,6 @@ telescope.setup{
       override_file_sorter = true,     -- override the file sorter
       case_mode = "smart_case",        -- or "ignore_case" or "respect_case", the default case_mode is "smart_case"
     },
-    media_files = {
-      filetypes = {"png", "webp", "jpg", "jpeg", "gif", "svg"},
-      -- find_cmd = "rg" -- find command (defaults to `fd`)
-    },
   }
 }
 
