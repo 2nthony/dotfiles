ce25bf1adc4b64408fef8bb670808f0f09cf8514
2nthony <ant.hu@outlook.com>
2022-09-02 13:08:13 +0800

    fix(nvim): C-h ^

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -36,9 +36,9 @@ keymap("n", ";Q", ":q!<cr>", opts)
 keymap("n", ";w", ":w<cr>", opts)
 
 -- move cursor to start of line, better ^
-keymap('n', '<C-h>', '<Home>', opts)
-keymap('i', '<C-h>', '<Home>', opts)
-keymap('v', '<C-h>', '<Home>', opts)
+keymap('n', '<C-h>', '<Home>^', opts)
+keymap('i', '<C-h>', '<Home><Esc>^i', opts)
+keymap('v', '<C-h>', '<Home>^', opts)
 
 -- better $
 keymap('n', '<C-l>', '<End>', opts)
