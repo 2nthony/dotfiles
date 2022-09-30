a0f9209c695d099897901bb7f568eae470d82b05
2nthony <ant.hu@outlook.com>
2022-10-01 03:05:33 +0800

    feat(nvim): bufferline style

--- .config/nvim/after/plugin/bufferline.lua
+++ .config/nvim/after/plugin/bufferline.lua
@@ -7,7 +7,7 @@ end
 bufferline.setup({
   options = {
     mode = "tabs",
-    separator_style = 'slant',
+    separator_style = 'padded_slant',
     always_show_bufferline = false,
     show_buffer_close_icons = false,
     show_close_icon = false,
