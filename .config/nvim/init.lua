69905882ccf8e4737c6bcbed390b4436f15e35e8
2nthony <ant.hu@outlook.com>
2022-10-01 01:50:58 +0800

    refactor(nvim): move

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,14 @@ require("base")
 require("highlights")
 require("keymaps")
 require("plugins")
+
+local has = vim.fn.has
+local is_mac = has "macunix"
+local is_win = has "win32"
+
+if is_mac then
+  require('macos')
+end
+if is_win then
+  require('windows')
+end
--- .config/nvim/lua/highlights.lua
+++ .config/nvim/lua/highlights.lua
@@ -4,16 +4,3 @@ vim.opt.winblend = 0
 vim.opt.wildoptions = 'pum'
 vim.opt.pumblend = 0
 vim.opt.background = 'dark'
-
-local has = function(x)
-  return vim.fn.has(x) == 1
-end
-local is_mac = has "macunix"
-local is_win = has "win32"
-
-if is_mac then
-  require('macos')
-end
-if is_win then
-  require('windows')
-end
