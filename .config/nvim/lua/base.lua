baaa23558579133b1c9541a53b47c710036d8828
2nthony <ant.hu@outlook.com>
2022-10-12 20:01:54 +0800

    chore(nvim): format

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -1,6 +1,7 @@
 vim.cmd("autocmd!")
 
-vim.scriptencoding = 'utf-8' vim.opt.encoding = 'utf-8'
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
 
 vim.wo.number = true
