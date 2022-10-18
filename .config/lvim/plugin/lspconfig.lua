1bf28491245a6f759fc58751756167aac1c44add
2nthony <ant.hu@outlook.com>
2022-10-18 20:40:48 +0800

    fix(lvim): lspconfig, volar check root dir `node_modules/vue`

--- .config/lvim/plugin/lspconfig.lua
+++ .config/lvim/plugin/lspconfig.lua
@@ -1,10 +1,13 @@
 -- builtin
 
 local lsp = require("lspconfig")
+local util = require("lspconfig.util")
+local lutils = require("lvim.utils")
 
 -- unmap all preset buffer keymaps, use lspsaga instead
 lvim.lsp.buffer_mappings.normal_mode = {}
 
 lsp.volar.setup {
-  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' },
+  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+  root_dir = util.root_pattern(lutils.join_paths("node_modules", "vue")) or util.root_pattern("package.json")
 }
