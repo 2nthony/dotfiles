b56f842f0756d188e9b64dff8711e04ce8ff4aba
2nthony <ant.hu@outlook.com>
2022-10-28 22:13:30 +0800

    chore(lvim): update

--- .config/lvim/lua/autocmds.lua
+++ .config/lvim/lua/autocmds.lua
@@ -1,6 +1,7 @@
 -- continuous comment next line
--- https://github.com/LunarVim/LunarVim/issues/3237
-vim.api.nvim_clear_autocmds { pattern = "*", group = "_format_options" }
+-- issue in https://github.com/LunarVim/LunarVim/issues/3237
+-- fixed in https://github.com/LunarVim/LunarVim/pull/3278
+-- vim.api.nvim_clear_autocmds { pattern = "*", group = "_format_options" }
 
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
