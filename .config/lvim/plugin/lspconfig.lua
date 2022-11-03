8909421c7b9fedf0af3690db26323e8f61f9a679
2nthony <ant.hu@outlook.com>
2022-11-03 10:24:46 +0800

    fix(lvim): lspconfig disable auto install servers

--- .config/lvim/plugin/lspconfig.lua
+++ .config/lvim/plugin/lspconfig.lua
@@ -7,6 +7,9 @@ local lutils = require("lvim.utils")
 -- unmap all preset buffer keymaps, use lspsaga instead
 lvim.lsp.buffer_mappings.normal_mode = {}
 
+-- disable auto install servers
+lvim.lsp.installer.setup.automatic_installation = nil
+
 lsp.volar.setup {
   filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
   root_dir = util.root_pattern(lutils.join_paths("node_modules", "vue")) or util.root_pattern("package.json")
