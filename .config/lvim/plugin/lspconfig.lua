e65862109a4b73d38fc652c94f71297accac10b0
2nthony <ant.hu@outlook.com>
2022-11-04 22:20:55 +0800

    fix(lvim): volar take over mode

--- .config/lvim/plugin/lspconfig.lua
+++ .config/lvim/plugin/lspconfig.lua
@@ -1,8 +1,6 @@
 -- builtin
 
 local lsp = require("lspconfig")
-local util = require("lspconfig.util")
-local lutils = require("lvim.utils")
 
 -- unmap all preset buffer keymaps, use lspsaga instead
 lvim.lsp.buffer_mappings.normal_mode = {}
@@ -11,6 +9,5 @@ lvim.lsp.buffer_mappings.normal_mode = {}
 lvim.lsp.installer.setup.automatic_installation = nil
 
 lsp.volar.setup {
-  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
-  root_dir = util.root_pattern(lutils.join_paths("node_modules", "vue")) or util.root_pattern("package.json")
+  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' },
 }
