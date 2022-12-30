3d8df5b8e303aed48f5bd43205690b5186ae9219
2nthony <ant.hu@outlook.com>
2022-12-30 22:17:31 +0800

    refactor(lvim): move bufferline highlight

--- .config/lvim/plugin/bufferline.lua
+++ .config/lvim/plugin/bufferline.lua
@@ -1,6 +1,11 @@
 -- builtin
 -- https://github.com/akinsho/bufferline.nvim
 
+local ok = pcall(require, "bufferline")
+if not ok then
+  return
+end
+
 local bufferline = lvim.builtin.bufferline
 
 bufferline.options = {
@@ -10,4 +15,25 @@ bufferline.options = {
   show_buffer_close_icons = false,
   show_close_icon = false,
   color_icons = true
+}
+
+bufferline.highlights = {
+  separator = {
+    fg = '#073642',
+    bg = '#002b36',
+  },
+  separator_selected = {
+    fg = '#073642',
+  },
+  background = {
+    fg = '#657b83',
+    bg = '#002b36'
+  },
+  buffer_selected = {
+    fg = '#fdf6e3',
+    bold = true,
+  },
+  fill = {
+    bg = '#073642'
+  },
 }
--- .config/lvim/plugin/neosolarized.lua
+++ .config/lvim/plugin/neosolarized.lua
@@ -37,26 +37,4 @@ Group.new("DiagnosticUnderlineWarn", colors.none, colors.none, styles.undercurl,
 Group.new("DiagnosticUnderlineInfo", colors.none, colors.none, styles.undercurl, cInfo)
 Group.new("DiagnosticUnderlineHint", colors.none, colors.none, styles.undercurl, cHint)
 
-local bufferline = lvim.builtin.bufferline
-bufferline.highlights = {
-  separator = {
-    fg = '#073642',
-    bg = '#002b36',
-  },
-  separator_selected = {
-    fg = '#073642',
-  },
-  background = {
-    fg = '#657b83',
-    bg = '#002b36'
-  },
-  buffer_selected = {
-    fg = '#fdf6e3',
-    bold = true,
-  },
-  fill = {
-    bg = '#073642'
-  },
-}
-
 vim.cmd('colorscheme neosolarized')
