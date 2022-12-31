a0b9b9fab40372e6453ff4d4604116f97d53a592
2nthony <ant.hu@outlook.com>
2022-12-31 18:47:51 +0800

    fix(lvim): lsp, tailwind root dir match

--- .config/lvim/plugin/0_lspconfig.lua
+++ .config/lvim/plugin/0_lspconfig.lua
@@ -1,7 +1,7 @@
 -- builtin
 
 local lsp = require("lspconfig")
-local util = require'lspconfig.util'
+local util = require 'lspconfig.util'
 
 -- unmap all preset buffer keymaps, use lspsaga instead
 lvim.lsp.buffer_mappings.normal_mode = {}
@@ -11,6 +11,5 @@ lvim.lsp.installer.setup.automatic_installation = nil
 
 lsp.volar.setup {}
 lsp.tailwindcss.setup {
-  root_dir = util.root_pattern('tailwind.config.js', 'tailwind.config.ts')
+  root_dir = util.root_pattern('tailwind.config.js', 'tailwind.config.cjs', 'tailwind.config.ts')
 }
-
