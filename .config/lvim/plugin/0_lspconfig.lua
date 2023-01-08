572702ea09e118f8c8932ceb4cef0a7472dcadad
2nthony <ant.hu@outlook.com>
2023-01-09 02:30:38 +0800

    fix(lvim): lsp volar take over mode

--- .config/lvim/plugin/0_lspconfig.lua
+++ .config/lvim/plugin/0_lspconfig.lua
@@ -1,7 +1,10 @@
 -- builtin
 
 local lsp = require("lspconfig")
-local util = require 'lspconfig.util'
+local util = require("lspconfig.util")
+
+local cwd = vim.fn.getcwd()
+local project_root = util.find_node_modules_ancestor(cwd)
 
 -- unmap all preset buffer keymaps, use lspsaga instead
 lvim.lsp.buffer_mappings.normal_mode = {}
@@ -9,7 +12,27 @@ lvim.lsp.buffer_mappings.normal_mode = {}
 -- disable auto install servers
 lvim.lsp.installer.setup.automatic_installation = nil
 
-lsp.volar.setup {}
 lsp.tailwindcss.setup {
   root_dir = util.root_pattern('tailwind.config.js', 'tailwind.config.cjs', 'tailwind.config.ts')
 }
+
+-- vue
+-- enable take over mode, disable tsserver
+local vue_path = util.path.join(project_root, "node_modules", "vue")
+local is_vue = vim.fn.isdirectory(vue_path) == 1
+if is_vue then
+  lsp.volar.setup {
+    filetypes = {
+      'vue',
+      'javascript',
+      'typescript',
+      'javascriptreact',
+      'typescriptreact',
+      'json',
+    },
+  }
+  lsp.tsserver.setup {
+    root_dir = function() return false end,
+    single_file_support = false,
+  }
+end
