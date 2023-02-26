162b4ad5744ff5b2a42cab7f30c435bba75602e1
2nthony <ant.hu@outlook.com>
2023-02-26 19:03:05 +0800

    feat(lvim): use persistence instead auto-session

---
+++ .config/lvim/lua/plugins/persistence.lua
@@ -0,0 +1,15 @@
+-- general
+-- https://github.com/folke/persistence.nvim
+
+return {
+  "folke/persistence.nvim",
+  opts = {
+    options = {
+      "buffers",
+      "curdir",
+      "tabpages",
+      "winsize",
+      "help",
+    },
+  },
+}
