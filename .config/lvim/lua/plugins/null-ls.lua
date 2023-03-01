77ed9aed64ef6e4751a3dc7f7460521994cabdfd
2nthony <ant.hu@outlook.com>
2023-03-01 22:42:03 +0800

    feat(lvim): null-ls, switched to eslint_d

--- .config/lvim/lua/plugins/null-ls.lua
+++ .config/lvim/lua/plugins/null-ls.lua
@@ -1,9 +1,17 @@
 -- builtin
 -- https://github.com/jose-elias-alvarez/null-ls.nvim
 
-local formatters = require("lvim.lsp.null-ls.formatters")
-formatters.setup({
-  {
-    command = "prettierd",
+local ok, null_ls = pcall(require, "null-ls")
+if not ok then
+  return
+end
+
+vim.lsp.buf.format({ timeout_ms = 5000 })
+
+null_ls.setup {
+  sources = {
+    null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.code_actions.eslint_d,
   },
-})
+}
