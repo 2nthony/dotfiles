d646be53a57ff80e0da28fa5777a4b3ee01853e8
2nthony <ant.hu@outlook.com>
2023-03-04 14:07:49 +0800

    fix(lvim): which key, tweak

--- .config/lvim/lua/plugins/which-key.lua
+++ .config/lvim/lua/plugins/which-key.lua
@@ -1,6 +1,8 @@
 local float_opts = require("utils.float-opts")
 local which_key = lvim.builtin.which_key
 
+which_key.setup.icons = nil
+
 which_key.setup.window.border = float_opts.border
 which_key.setup.window.winblend = float_opts.winblend
-which_key.setup.window.padding = { 0, 0, 0, 0 }
+which_key.setup.window.padding = { 0, 0, 2, 0 }
