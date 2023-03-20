a4b5764de1f30df0da86567791cb0b087c7bd98c
2nthony <ant.hu@outlook.com>
2023-03-21 02:24:51 +0800

    alpha

--- .config/nvim/lua/plugins/editor/alpha.lua
+++ .config/nvim/lua/plugins/editor/alpha.lua
@@ -1,11 +1,18 @@
 -- https://github.com/goolord/alpha-nvim
-
-local icons = require("util.icon")
+-- https://github.com/ayamir/nvimdots/blob/main/lua/modules/configs/ui/alpha.lua
 
 return {
   "goolord/alpha-nvim",
+  lazy = true,
+  event = function()
+    return { "BufWinEnter" }
+  end,
   opts = function(_, dashboard)
-    dashboard.section.header.val = " "
+    local icons = require("lazyvim.config").icons
+    local logo = require("util.alpha").logo
+
+    dashboard.section.header.val = logo.empty
+
     dashboard.section.buttons.val = {
       dashboard.button("n", icons.ui.NewFile .. "  New File", ":ene! <cr>"),
       dashboard.button("ff", icons.ui.FindFile .. "  Find File", ":Telescope find_files <cr>"),
@@ -16,20 +23,33 @@ return {
       ),
       dashboard.button("s", icons.ui.History .. "  Restore sessions", ":lua require('persistence').load()<cr>"),
       dashboard.button("c", icons.ui.Gear .. "  Config", ":e $MYVIMRC <cr>"),
-
       dashboard.button("l", icons.misc.Lazy .. "  Lazy", ":Lazy<cr>"),
       dashboard.button("m", icons.ui.Package .. "  Mason", ":Mason<cr>"),
       dashboard.button("q", icons.ui.Close .. "  Quit", ":q<cr>"),
       dashboard.button("x", icons.ui.SignOut .. "  Exit (Quit all)", ":qa<cr>"),
     }
-
     for _, button in ipairs(dashboard.section.buttons.val) do
       button.opts.hl = "AlphaButton"
       button.opts.hl_shortcut = "AlphaShortcut"
     end
 
-    dashboard.opts.layout[1].val = 2
+    local head_butt_padding = 2
+    local occu_height = #dashboard.section.header.val + 2 * #dashboard.section.buttons.val + head_butt_padding
+    local header_padding = math.max(0, math.ceil((vim.fn.winheight(0) - occu_height) * 0.25))
+    local foot_butt_padding = 1
+
+    dashboard.opts.layout = {
+      { type = "padding", val = header_padding },
+      dashboard.section.header,
+      { type = "padding", val = head_butt_padding },
+      dashboard.section.buttons,
+      { type = "padding", val = foot_butt_padding },
+      dashboard.section.footer,
+    }
 
     return dashboard
+  end,
+  config = function(_, dashboard)
+    require("alpha").setup(dashboard.opts)
   end,
 }
