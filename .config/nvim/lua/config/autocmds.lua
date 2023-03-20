a778febd21c99379af0470642179754d774ed046
2nthony <ant.hu@outlook.com>
2023-03-20 10:48:13 +0800

    copy autocmds

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Turn off paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  command = "set nopaste",
+})
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = ".*rc",
+  command = "setfiletype json",
+})
