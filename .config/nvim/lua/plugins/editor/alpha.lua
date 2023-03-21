3ef3d8cbac547993a315728bfc1fbdb8a28e4bc1
2nthony <ant.hu@outlook.com>
2023-03-21 11:18:32 +0800

    alpha, icons

--- .config/nvim/lua/plugins/editor/alpha.lua
+++ .config/nvim/lua/plugins/editor/alpha.lua
@@ -22,11 +22,11 @@ return {
         ":lua require('neo-tree.command').execute({ toggle = true }) <cr>"
       ),
       dashboard.button("s", icons.ui.History .. "  Restore sessions", ":lua require('persistence').load()<cr>"),
-      dashboard.button("c", icons.ui.Gear .. "  Config", ":e $MYVIMRC <cr>"),
       dashboard.button("l", icons.misc.Lazy .. "  Lazy", ":Lazy<cr>"),
       dashboard.button("m", icons.ui.Package .. "  Mason", ":Mason<cr>"),
-      dashboard.button("q", icons.ui.Close .. "  Quit", ":q<cr>"),
-      dashboard.button("x", icons.ui.SignOut .. "  Exit (Quit all)", ":qa<cr>"),
+      dashboard.button("c", icons.ui.Gear .. "  Config", ":e $MYVIMRC <cr>"),
+      dashboard.button("q", icons.ui.SignOut .. "  Quit", ":q<cr>"),
+      dashboard.button("x", icons.ui.CloseAll .. "  Exit (Quit all)", ":qa<cr>"),
     }
     for _, button in ipairs(dashboard.section.buttons.val) do
       button.opts.hl = "AlphaButton"
