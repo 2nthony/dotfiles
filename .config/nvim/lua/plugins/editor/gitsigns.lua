75bc3d5498ae3a4af08e313834e7814ca0ae5f86
2nthony <ant.hu@outlook.com>
2023-03-22 15:41:40 +0800

    gitsigns border

--- .config/nvim/lua/plugins/editor/gitsigns.lua
+++ .config/nvim/lua/plugins/editor/gitsigns.lua
@@ -6,9 +6,15 @@ return {
   lazy = true,
   cmd = { "Gitsigns" },
   opts = function(_, opts)
+    local float = require("util.opts").float
+
     return {
       signs = opts.signs,
       current_line_blame = true,
+      preview_config = {
+        border = float.border,
+        style = float.style,
+      },
     }
   end,
 }
