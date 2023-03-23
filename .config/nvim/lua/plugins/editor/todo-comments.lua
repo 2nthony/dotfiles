b73bfba216035d83bd8b4c4e49eb4a3605ee182d
2nthony <ant.hu@outlook.com>
2023-03-23 14:50:39 +0800

    todo comments, update keywords icons

--- .config/nvim/lua/plugins/editor/todo-comments.lua
+++ .config/nvim/lua/plugins/editor/todo-comments.lua
@@ -1,10 +1,36 @@
 -- https://github.com/folke/todo-comments.nvim
 
+local icons = require("config.icons")
+
 return {
   "folke/todo-comments.nvim",
   cmd = { "TodoLocList", "TodoTrouble", "TodoTelescope" },
   keys = function()
     return {}
   end,
-  config = true,
+  opts = {
+    keywords = {
+      FIX = {
+        icon = icons.ui.Bug,
+      },
+      TODO = {
+        icon = icons.ui.Check,
+      },
+      HACK = {
+        icon = icons.ui.Skull,
+      },
+      WARN = {
+        icon = icons.diagnostics.Warn,
+      },
+      PERF = {
+        icon = icons.ui.Dashboard,
+      },
+      NOTE = {
+        icon = icons.ui.Comment,
+      },
+      TEST = {
+        icon = icons.ui.Beaker,
+      },
+    },
+  },
 }
