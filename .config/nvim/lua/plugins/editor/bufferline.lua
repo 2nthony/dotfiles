55b25494b9cba1ce164a68a34aac9e0be5196edd
2nthony <ant.hu@outlook.com>
2023-04-05 14:14:06 +0800

    fix bufferline using vitesse features

--- .config/nvim/lua/plugins/editor/bufferline.lua
+++ .config/nvim/lua/plugins/editor/bufferline.lua
@@ -1,25 +1,30 @@
-local slant = require("vitesse.features.bufferline.slant")
-
 return {
   "akinsho/bufferline.nvim",
-  dependencies = { "2nthony/vitesse.nvim" },
   event = function()
     return { "BufReadPost" }
   end,
   keys = function()
     return {}
   end,
-  opts = {
-    options = {
+  opts = function(_, opts)
+    local has = require("lazyvim.util").has
+
+    opts.options = vim.tbl_extend("force", opts.options, {
       diagnostics = "nvim_lsp",
       mode = "tabs",
-      separator_style = slant.separator_style,
       always_show_bufferline = false,
       show_buffer_close_icons = false,
       show_close_icon = false,
       color_icons = true,
       show_duplicate_prefix = false,
-    },
-    highlights = slant.highlights,
-  },
+    })
+
+    if has("vitesse.nvim") then
+      local slant = require("vitesse.features.bufferline.slant")
+      opts.options.separator_style = slant.separator_style
+      opts.highlights = slant.highlights
+    end
+
+    return opts
+  end,
 }
