15f52c6b8dd9b924a4dd3a53d2c5e4cc34fb9cd0
2nthony <ant.hu@outlook.com>
2023-04-05 13:51:58 +0800

    update

--- .config/nvim/lua/plugins/colorscheme/vitesse.lua
+++ .config/nvim/lua/plugins/colorscheme/vitesse.lua
@@ -1,17 +1,15 @@
 return {
-  {
-    "2nthony/vitesse.nvim",
-    dir = "~/ghq/github.com/2nthony/vitesse.nvim",
-    lazy = true,
-    -- priority = 1000,
-    dependencies = {
-      "tjdevries/colorbuddy.nvim",
-    },
-    opts = {
-      reverse_visual = true,
-      dim_nc = true,
-      -- transparent_background = false,
-      -- transparent_float_background = false,
-    },
+  "2nthony/vitesse.nvim",
+  dir = "~/ghq/github.com/2nthony/vitesse.nvim",
+  lazy = true,
+  -- priority = 1000,
+  dependencies = {
+    "tjdevries/colorbuddy.nvim",
+  },
+  opts = {
+    reverse_visual = true,
+    dim_nc = true,
+    -- transparent_background = false,
+    -- transparent_float_background = false,
   },
 }
