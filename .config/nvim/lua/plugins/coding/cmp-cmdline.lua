b9b98034e45074e0f37ef4b0f115d1616b840722
2nthony <ant.hu@outlook.com>
2023-04-05 19:08:37 +0800

    cmp cmdline

---
+++ .config/nvim/lua/plugins/coding/cmp-cmdline.lua
@@ -0,0 +1,35 @@
+return {
+  "hrsh7th/cmp-cmdline",
+  lazy = true,
+  event = { "CmdlineEnter" },
+  opts = {
+    {
+      type = ":",
+      sources = {
+        { name = "path" },
+        { name = "cmdline" },
+      },
+    },
+    {
+      type = { "/", "?" },
+      sources = {
+        { name = "buffer" },
+      },
+    },
+  },
+  dependencies = {
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+  },
+  config = function(_, opts)
+    local cmp = require("cmp")
+
+    for _, opt in ipairs(opts) do
+      cmp.setup.cmdline(opt.type, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources(opt.sources),
+      })
+    end
+  end,
+}
