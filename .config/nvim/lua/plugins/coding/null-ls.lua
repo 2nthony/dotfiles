b4821979a339d5fd1b883f3d6c04d9549332a0b4
2nthony <ant.hu@outlook.com>
2023-04-10 02:11:51 +0800

    use lazyvim eslint config

--- .config/nvim/lua/plugins/coding/null-ls.lua
+++ .config/nvim/lua/plugins/coding/null-ls.lua
@@ -8,25 +8,26 @@ local prettier_files = {
 }
 
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  lazy = true,
-  opts = function(_, opts)
-    local float = require("util.opts").float
-    local nls = require("null-ls")
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    lazy = true,
+    opts = function(_, opts)
+      local float = require("util.opts").float
+      local nls = require("null-ls")
 
-    return vim.tbl_extend("force", opts, {
-      log_level = "error",
-      border = float.border,
-      sources = vim.list_extend(opts.sources, {
-        nls.builtins.formatting.prettier.with({
-          condition = function(utils)
-            return utils.root_has_file(prettier_files)
-          end,
+      return vim.tbl_extend("force", opts, {
+        log_level = "error",
+        border = float.border,
+        sources = vim.list_extend(opts.sources, {
+          nls.builtins.formatting.prettier.with({
+            condition = function(utils)
+              return utils.root_has_file(prettier_files)
+            end,
+          }),
         }),
-        nls.builtins.formatting.eslint_d,
-        nls.builtins.diagnostics.eslint_d,
-        nls.builtins.code_actions.eslint_d,
-      }),
-    })
-  end,
+      })
+    end,
+  },
+  -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/extras/linting/eslint.lua
+  { import = "lazyvim.plugins.extras.linting.eslint" },
 }
