f04a2aad38094d543f65f084265bda72c22011c3
2nthony <ant.hu@outlook.com>
2023-04-13 17:03:50 +0800

    add options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,10 +5,15 @@
 local float = require("util.opts").float
 local opt = vim.opt
 
+opt.backup = false
+opt.backupskip = { "/tmp/*", "/private/tmp/*" }
+opt.breakindent = true
+opt.formatoptions:append({ "r" })
 opt.mouse = ""
 opt.relativenumber = false
 opt.diffopt = "filler,iwhite,internal,linematch:60,algorithm:patience"
 
+-- popup, float
 opt.winblend = float.winblend
 opt.pumblend = float.pumblend
 opt.pumheight = 10
