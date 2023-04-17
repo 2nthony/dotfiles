7db8f0e857617e8f30b76fb80e7b03c624011d8a
2nthony <ant.hu@outlook.com>
2023-04-17 12:19:00 +0800

    Add float options to lazygit terminal in git.lua

--- .config/nvim/lua/util/git.lua
+++ .config/nvim/lua/util/git.lua
@@ -1,4 +1,5 @@
 local has = require("lazyvim.util").has
+local float = require("util.opts").float
 
 local M = {}
 
@@ -8,12 +9,18 @@ M.lazygit = function()
     return
   end
 
+  local size = float.get_size("full")
   local Terminal = require("toggleterm.terminal").Terminal
   local lazygit = Terminal:new({
     cmd = "lazygit",
     hidden = true,
     -- direction = toggleterm.direction,
     -- float_opts = toggleterm.float_opts,
+    float_opts = {
+      width = size.width,
+      height = size.height,
+      border = "none",
+    },
   })
 
   lazygit:toggle()
