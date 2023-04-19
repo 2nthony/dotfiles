f2cec5463f98c2876ecb611e68c53292f952d574
2nthony <ant.hu@outlook.com>
2023-04-19 16:11:34 +0800

    refactor bufferline

--- .config/nvim/lua/plugins/editor/bufferline.lua
+++ .config/nvim/lua/plugins/editor/bufferline.lua
@@ -1,27 +1,29 @@
 return {
-  "akinsho/bufferline.nvim",
-  keys = function()
-    return {}
-  end,
-  opts = function(_, opts)
-    local has = require("lazyvim.util").has
-
-    opts.options = vim.tbl_extend("force", opts.options, {
-      diagnostics = "nvim_lsp",
-      mode = "tabs",
-      always_show_bufferline = false,
-      show_buffer_close_icons = false,
-      show_close_icon = false,
-      color_icons = true,
-      show_duplicate_prefix = false,
-    })
-
-    if has("vitesse.nvim") then
-      local slant = require("vitesse.features.bufferline.slant")
-      opts.options.separator_style = "padded_slant"
-      opts.highlights = slant.highlights
-    end
-
-    return opts
-  end,
+  {
+    "akinsho/bufferline.nvim",
+    keys = function()
+      return {}
+    end,
+    opts = {
+      options = {
+        diagnostics = "nvim_lsp",
+        mode = "tabs",
+        always_show_bufferline = false,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        color_icons = true,
+        -- show_duplicate_prefix = false,
+      },
+    },
+  },
+  {
+    "bufferline.nvim",
+    dependencies = "vitesse.nvim",
+    opts = {
+      options = {
+        separator_style = "padded_slant",
+      },
+      highlights = require("vitesse.features.bufferline.slant").highlights,
+    },
+  },
 }
