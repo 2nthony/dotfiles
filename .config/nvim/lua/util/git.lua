70ce2f8883fe6cbc37072198ccfeb4510af7b108
2nthony <ant.hu@outlook.com>
2023-05-08 14:21:53 +0800

    util git support args

--- .config/nvim/lua/util/git.lua
+++ .config/nvim/lua/util/git.lua
@@ -3,7 +3,8 @@ local float = require("util.opts").float
 
 local M = {}
 
-M.lazygit = function()
+---@param termArgs? TermCreateArgs
+M.lazygit = function(termArgs)
   if not has("toggleterm.nvim") then
     vim.notify("toggleterm not found!", "error")
     return
@@ -11,17 +12,16 @@ M.lazygit = function()
 
   local size = float.get_size("full")
   local Terminal = require("toggleterm.terminal").Terminal
-  local lazygit = Terminal:new({
+
+  local lazygit = Terminal:new(vim.tbl_extend("force", termArgs or {}, {
     cmd = "lazygit",
     hidden = true,
-    -- direction = toggleterm.direction,
-    -- float_opts = toggleterm.float_opts,
     float_opts = {
       width = size.width,
       height = size.height,
       border = "none",
     },
-  })
+  }))
 
   lazygit:toggle()
 end
