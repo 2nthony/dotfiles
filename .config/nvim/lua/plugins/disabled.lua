122bdbea2f317295e5f93d51c06681f07a65c37f
2nthony <ant.hu@outlook.com>
2023-05-09 02:09:56 +0800

    feat(nvim): enable vim-illuminate

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,7 +2,6 @@ local disabled = {
   "ggandor/flit.nvim",
   "SmiteshP/nvim-navic",
   "rcarriga/nvim-notify",
-  "RRethy/vim-illuminate",
   "echasnovski/mini.bufremove",
   "catppuccin", -- lazyvim renamed
   "folke/tokyonight.nvim",
