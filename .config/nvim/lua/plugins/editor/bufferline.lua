b961982b782b29369ab4f0779fa61d9f72273566
2nthony <ant.hu@outlook.com>
2023-05-09 10:59:58 +0800

    fix(nvim): bufferline, pin icon

--- .config/nvim/lua/plugins/editor/bufferline.lua
+++ .config/nvim/lua/plugins/editor/bufferline.lua
@@ -1,3 +1,6 @@
+local icons = require("config.icons")
+local icons_ui = icons.get("ui")
+
 return {
   {
     "akinsho/bufferline.nvim",
@@ -13,6 +16,11 @@ return {
         show_close_icon = false,
         color_icons = true,
         -- show_duplicate_prefix = false,
+        groups = {
+          items = {
+            require("bufferline.groups").builtin.pinned:with({ icon = icons_ui.Pinned }),
+          },
+        },
       },
     },
   },
