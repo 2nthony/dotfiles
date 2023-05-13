0691159569786709915e71044a75a60f688f38ca
2nthony <ant.hu@outlook.com>
2023-05-13 22:20:54 +0800

    chore(nvim): use local `lazyvimp` repo

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -15,7 +15,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    { "2nthony/lazyvimp" },
+    { "2nthony/lazyvimp", dir = "~/ghq/github.com/2nthony/lazyvimp" },
     -- import/override with your plugins
     { import = "plugins" },
   },
