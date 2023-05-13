62fda3eabde4144b824a6f56139c06e7581e9997
2nthony <ant.hu@outlook.com>
2023-05-13 17:04:10 +0800

    update

--- .config/nvim/lua/plugins/editor/bufferline.lua
+++ .config/nvim/lua/plugins/editor/bufferline.lua
@@ -2,21 +2,19 @@ local icons = require("config.icons")
 local icons_ui = icons.get("ui")
 
 return {
-  {
-    "akinsho/bufferline.nvim",
-    opts = {
-      options = {
-        diagnostics = "nvim_lsp",
-        mode = "tabs",
-        always_show_bufferline = false,
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-        color_icons = true,
-        -- show_duplicate_prefix = false,
-        groups = {
-          items = {
-            require("bufferline.groups").builtin.pinned:with({ icon = icons_ui.Pinned }),
-          },
+  "akinsho/bufferline.nvim",
+  opts = {
+    options = {
+      diagnostics = "nvim_lsp",
+      mode = "tabs",
+      always_show_bufferline = false,
+      show_buffer_close_icons = false,
+      show_close_icon = false,
+      color_icons = true,
+      -- show_duplicate_prefix = false,
+      groups = {
+        items = {
+          require("bufferline.groups").builtin.pinned:with({ icon = icons_ui.Pinned }),
         },
       },
     },
