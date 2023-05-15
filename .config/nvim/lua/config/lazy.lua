2130c3032c04e2edde69f341a4255355ecb3fd3f
2nthony <ant.hu@outlook.com>
2023-05-15 13:30:16 +0800

    fix(nvim): safe load local.lua

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -17,7 +17,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
-    pcall(require, "local") and { import = "local" },
+    pcall(require, "local") and { import = "local" } or nil,
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
