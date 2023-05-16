9e7381a1dd22e342759530d2508841d326a004dc
2nthony <ant.hu@outlook.com>
2023-05-16 22:54:37 +0800

    feat(nvim): save transparent as an extra

---
+++ .config/nvim/lua/plugins/extras/ui/transparent.lua
@@ -0,0 +1,123 @@
+vim.opt.pumblend = 0
+vim.opt.winblend = 0
+
+local float = require("util.opts").float
+
+return {
+  {
+    "vitesse.nvim",
+    opts = {
+      transparent_background = true,
+      transparent_float_background = true,
+    },
+  },
+
+  -- editor
+  {
+    "gitsigns.nvim",
+    opts = {
+      preview_config = {
+        border = float.border,
+      },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      window = {
+        border = float.border,
+        winblend = float.winblend,
+      },
+    },
+  },
+
+  -- coding
+  {
+    "nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      local win = require("util.opts").win
+
+      local win_bordered = cmp.config.window.bordered({
+        border = float.border,
+        winhighlight = win.winhighlight,
+      })
+
+      opts.window = {
+        completion = win_bordered,
+        documentation = win_bordered,
+      }
+    end,
+  },
+
+  -- lsp
+  {
+    "lspsaga.nvim",
+    opts = {
+      ui = {
+        border = float.border,
+        winblend = float.winblend,
+      },
+    },
+  },
+  {
+    "nvim-lspconfig",
+    opts = {
+      diagnostics = {
+        float = {
+          border = float.border,
+        },
+      },
+    },
+  },
+  {
+    "nvim-lspconfig",
+    opts = function()
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, float)
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, float)
+
+      require("lspconfig.ui.windows").default_options = {
+        border = float.border,
+      }
+    end,
+  },
+  {
+    "mason.nvim",
+    opts = {
+      ui = {
+        border = float.border,
+      },
+    },
+  },
+  {
+    "null-ls.nvim",
+    opts = {
+      border = float.border,
+    },
+  },
+
+  -- dap
+  {
+    "overseer.nvim",
+    opts = {
+      form = {
+        border = float.border,
+        win_opts = {
+          winblend = float.winblend,
+        },
+      },
+      confirm = {
+        border = float.border,
+        win_opts = {
+          winblend = float.winblend,
+        },
+      },
+      task_win = {
+        border = float.border,
+        win_opts = {
+          winblend = float.winblend,
+        },
+      },
+    },
+  },
+}
