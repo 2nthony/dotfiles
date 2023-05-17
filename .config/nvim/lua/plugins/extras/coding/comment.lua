1bfb3fc2df2c0e563fc08b31ed59b703744308cf
2nthony <ant.hu@outlook.com>
2023-05-17 14:09:36 +0800

    feat(nvim): add comment extra

---
+++ .config/nvim/lua/plugins/extras/coding/comment.lua
@@ -0,0 +1,29 @@
+-- https://github.com/LazyVim/LazyVim/releases/tag/v3.0.0
+-- https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#commentnvim
+-- https://github.com/ayamir/nvimdots/blob/main/lua/modules/configs/editor/comment.lua
+
+return {
+  { "echasnovski/mini.comment", enabled = false },
+
+  {
+    "nvim-treesitter",
+    opts = {
+      context_commentstring = {
+        enable = true,
+        enable_autocmd = false,
+      },
+    },
+  },
+  {
+    "numToStr/Comment.nvim",
+    lazy = true,
+    event = { "VeryLazy" },
+    opts = function(_, opts)
+      opts.ignore = "^$"
+      opts.pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+    end,
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+  },
+}
