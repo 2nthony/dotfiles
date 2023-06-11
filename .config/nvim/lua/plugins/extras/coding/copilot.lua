cec03bee950a57bf90ad8b427607278decec2d36
2nthony <ant.hu@outlook.com>
2023-06-11 13:23:08 +0800

    feat(nvim): copilot, use code lens, disable cmp

---
+++ .config/nvim/lua/plugins/extras/coding/copilot.lua
@@ -0,0 +1,21 @@
+return {
+  { import = "lazyvim.plugins.extras.coding.copilot" },
+  {
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+      },
+      filetypes = {
+        vue = true,
+      },
+    },
+  },
+  -- disable cmp
+  {
+    "copilot-cmp",
+    pin = true,
+    config = false,
+  },
+}
