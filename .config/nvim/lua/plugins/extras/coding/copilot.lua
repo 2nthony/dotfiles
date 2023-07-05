2ea4a3c2d0e84e637e7268185378a4652eb175bc
2nthony <ant.hu@outlook.com>
2023-07-05 23:16:52 +0800

    refactor(nvim): copilot, move tab back to copilot itself

--- .config/nvim/lua/plugins/extras/coding/copilot.lua
+++ .config/nvim/lua/plugins/extras/coding/copilot.lua
@@ -15,6 +15,22 @@ return {
   {
     "zbirenbaum/copilot.lua",
     event = "VeryLazy",
+    keys = {
+      {
+        "<tab>",
+        function()
+          local suggestion = require("copilot.suggestion")
+          if suggestion.is_visible() then
+            return '<cmd>lua require("copilot.suggestion").accept()<cr>'
+          else
+            return "<Tab>"
+          end
+        end,
+        mode = { "i" },
+        expr = true,
+        silent = true,
+      },
+    },
     opts = {
       suggestion = {
         enabled = true,
