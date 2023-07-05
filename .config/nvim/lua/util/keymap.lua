563992de29fc7d5e3dfcb26232ca3e8de2c0c445
2nthony <ant.hu@outlook.com>
2023-07-06 02:26:42 +0800

    fix(nvim): util keymap, should override-able

--- .config/nvim/lua/util/keymap.lua
+++ .config/nvim/lua/util/keymap.lua
@@ -1,15 +1,16 @@
 local M = {}
 
 M.map = function(mode, lhs, rhs, opts)
+  -- local keys = require("lazy.core.handler").handlers.keys
+  -- ---@cast keys LazyKeysHandler
+  -- -- do not create the keymap if a lazy keys handler exists
+  -- if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+  -- end
+
   rhs = rhs or "<nop>"
-  local keys = require("lazy.core.handler").handlers.keys
-  ---@cast keys LazyKeysHandler
-  -- do not create the keymap if a lazy keys handler exists
-  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
+  opts = opts or {}
+  opts.silent = opts.silent ~= false
+  vim.keymap.set(mode, lhs, rhs, opts)
 end
 
 M.del = function(modes, lhs, opts)
