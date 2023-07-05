a69f5e987f061905c8c1e1ef9cccabe324832350
2nthony <ant.hu@outlook.com>
2023-07-05 15:17:17 +0800

    refactor(nvim): keymap utils

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,20 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local map = require("util.keymap").map
+local lazykeys = require("util.keymap").lazykeys
 local ok, keymaps = pcall(require, "keymaps")
 if ok then
   local generals = keymaps.generals
-
-  for _, keymap in ipairs(generals) do
-    local mode = keymap.mode or "n"
-    local lhs = keymap[1]
-    local rhs = keymap[2]
-    local opts = {
-      desc = keymap.desc,
-      remap = keymap.remap,
-    }
-
-    map(mode, lhs, rhs, opts)
-  end
+  lazykeys(generals)
 end
--- .config/nvim/lua/util/keymap.lua
+++ .config/nvim/lua/util/keymap.lua
@@ -16,4 +16,25 @@ M.del = function(modes, lhs, opts)
   vim.keymap.del(modes, lhs, opts)
 end
 
+---@param key LazyKeys
+M.lazykey = function(key)
+  local mode = key.mode or "n"
+  local lhs = key[1]
+  local rhs = key[2]
+  local opts = {
+    desc = key.desc,
+    remap = key.remap,
+    expr = key.expr,
+  }
+
+  M.map(mode, lhs, rhs, opts)
+end
+
+---@param keys LazyKeys[]
+M.lazykeys = function(keys)
+  for _, key in ipairs(keys) do
+    M.lazykey(key)
+  end
+end
+
 return M
