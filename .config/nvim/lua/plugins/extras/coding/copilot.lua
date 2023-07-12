8cf0a6be5bf9fc3b6965e8d11b34b316e9ab3e09
2nthony <ant.hu@outlook.com>
2023-07-12 13:49:25 +0800

    feat(nvim): copilot, more keymaps

--- .config/nvim/lua/plugins/extras/coding/copilot.lua
+++ .config/nvim/lua/plugins/extras/coding/copilot.lua
@@ -30,11 +30,46 @@ return {
         expr = true,
         silent = true,
       },
+      {
+        "<m-right>",
+        function()
+          local suggestion = require("copilot.suggestion")
+          if suggestion.is_visible() then
+            return '<cmd>lua require("copilot.suggestion").accept_word()<cr>'
+          else
+            return "<m-right>"
+          end
+        end,
+        mode = { "i" },
+        expr = true,
+        silent = true,
+        desc = "Copilot accept word",
+      },
+      {
+        "<m-l>",
+        function()
+          local suggestion = require("copilot.suggestion")
+          if suggestion.is_visible() then
+            return '<cmd>lua require("copilot.suggestion").accept_line()<cr>'
+          else
+            return "<m-l>"
+          end
+        end,
+        mode = { "i" },
+        expr = true,
+        silent = true,
+        desc = "Copilot accept line",
+      },
     },
     opts = {
       suggestion = {
         enabled = true,
         auto_trigger = true,
+        keymap = {
+          accept = false,
+          -- accept_word = "<m-right>",
+          -- accept_line = "<m-l>",
+        },
       },
       filetypes = {
         vue = true,
