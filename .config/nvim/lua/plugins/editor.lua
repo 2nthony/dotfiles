9f02f0222a1bac75d2623bdf209fbbcb8d5efbf6
2nthony <ant.hu@outlook.com>
2023-07-30 13:58:13 +0800

    fix(nvim): flash, disable search integration

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -51,6 +51,9 @@ return {
     vscode = true,
     opts = {
       modes = {
+        search = {
+          enabled = false,
+        },
         -- disable flit, I use `f` for search/find
         char = {
           enabled = false,
