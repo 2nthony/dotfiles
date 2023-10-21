4a865c142789c71709db92c73957aebcb84bc713
2nthony <ant.hu@outlook.com>
2023-10-21 14:47:21 +0800

    refactor(nvim): move plugins

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -7,38 +7,6 @@ return {
   { "echasnovski/mini.bufremove", enabled = false },
 
   {
-    "sindrets/diffview.nvim",
-    lazy = true,
-    cmd = {
-      "DiffviewOpen",
-      "DiffviewClose",
-      "DiffviewLog",
-      "DiffviewRefresh",
-      "DiffviewToggleFiles",
-      "DiffviewFocusFiles",
-      "DiffviewFileHistory",
-    },
-    opts = function()
-      local keymap_q_close = { "n", "q", ":DiffviewClose<cr>", { desc = "Diffview Close" } }
-
-      return {
-        enhanced_diff_hl = true,
-        keymaps = {
-          view = {
-            keymap_q_close,
-          },
-          file_panel = {
-            keymap_q_close,
-          },
-          file_history_panel = {
-            keymap_q_close,
-          },
-        },
-      }
-    end,
-  },
-
-  {
     "folke/flash.nvim",
     vscode = true,
     opts = {
@@ -90,6 +58,15 @@ return {
         replace_confirm = "<cr>",
         replace_all = "<leader><cr>",
       },
+    },
+  },
+
+  {
+    "LintaoAmons/easy-commands.nvim",
+    event = "VeryLazy",
+    opts = {},
+    dependencies = {
+      "NeogitOrg/neogit",
     },
   },
 }
--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -13,4 +13,36 @@ return {
       current_line_blame = true,
     },
   },
+
+  {
+    "sindrets/diffview.nvim",
+    lazy = true,
+    cmd = {
+      "DiffviewOpen",
+      "DiffviewClose",
+      "DiffviewLog",
+      "DiffviewRefresh",
+      "DiffviewToggleFiles",
+      "DiffviewFocusFiles",
+      "DiffviewFileHistory",
+    },
+    opts = function()
+      local keymap_q_close = { "n", "q", ":DiffviewClose<cr>", { desc = "Diffview Close" } }
+
+      return {
+        enhanced_diff_hl = true,
+        keymaps = {
+          view = {
+            keymap_q_close,
+          },
+          file_panel = {
+            keymap_q_close,
+          },
+          file_history_panel = {
+            keymap_q_close,
+          },
+        },
+      }
+    end,
+  },
 }
