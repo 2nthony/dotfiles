66ac42d7375dccde8fea097b2192238b0ce8a487
2nthony <ant.hu@outlook.com>
2023-11-15 21:36:36 +0800

    fix(nvim): disable conceal for json, jsonc, markdown

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -28,3 +28,12 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.b.miniindentscope_disable = true
   end,
 })
+
+-- Disable the concealing in some file formats
+-- The default conceallevel is 3 in LazyVim
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json", "jsonc", "markdown" },
+  callback = function()
+    vim.opt.conceallevel = 0
+  end,
+})
