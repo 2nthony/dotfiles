12fef7af0d0db2c230d53883b38f99a6154890b0
2nthony <ant.hu@outlook.com>
2024-01-13 01:24:53 +0800

    fix(nvim): buffer, use tabs mode back

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -9,7 +9,7 @@ return {
     "akinsho/bufferline.nvim",
     opts = {
       options = {
-        -- mode = "tabs",
+        mode = "tabs",
         show_buffer_close_icons = false,
         show_close_icon = false,
         separator_style = "slant",
