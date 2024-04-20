3b75b90ffbb42543541a180fdbe966b89f4b82e5
2nthony <ant.hu@outlook.com>
2024-04-20 23:26:04 +0800

    feat(nvim): lsp, use typescript-tools.nvim for tsserver

--- .config/nvim/lua/plugins/extras/lang/typescript.lua
+++ .config/nvim/lua/plugins/extras/lang/typescript.lua
@@ -1,20 +1,29 @@
--- typescript.nvim archived
-
 return {
   { import = "lazyvim.plugins.extras.lang.typescript" },
 
   {
     "nvim-lspconfig",
     opts = {
-      servers = {
-        tsserver = {
-          settings = {
-            completions = {
-              completeFunctionCalls = true,
-            },
-          },
-        },
+      setup = {
+        -- return `true` don't setup with lspconfig
+        tsserver = function()
+          return true
+        end,
       },
+    },
+  },
+
+  {
+    "pmizio/typescript-tools.nvim",
+    opts = {
+      settings = {
+        complete_function_calls = true,
+        expose_as_code_action = "all",
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "neovim/nvim-lspconfig",
     },
   },
 }
