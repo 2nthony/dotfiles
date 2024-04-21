ead780d28092e5a9b7eb883c476ae6a48bf94846
2nthony <ant.hu@outlook.com>
2024-04-21 10:52:23 +0800

    feat(nvim): add Octo.nvim for github

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -45,4 +45,18 @@ return {
       }
     end,
   },
+
+  {
+    "pwntester/octo.nvim",
+    cmd = "Octo",
+    opts = {
+      default_to_projects_v2 = true,
+      default_merge_method = "squash",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
 }
