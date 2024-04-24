8c95003e4414fa1c31317665d06f31c020b0576d
2nthony <ant.hu@outlook.com>
2024-04-24 11:44:24 +0800

    refactor(nvim): move tsserver settings to lsp settings

--- .config/nvim/lua/plugins/extras/lang/typescript.lua
+++ .config/nvim/lua/plugins/extras/lang/typescript.lua
@@ -1,20 +1,3 @@
--- typescript.nvim archived
-
 return {
   { import = "lazyvim.plugins.extras.lang.typescript" },
-
-  {
-    "nvim-lspconfig",
-    opts = {
-      servers = {
-        tsserver = {
-          settings = {
-            completions = {
-              completeFunctionCalls = true,
-            },
-          },
-        },
-      },
-    },
-  },
 }
