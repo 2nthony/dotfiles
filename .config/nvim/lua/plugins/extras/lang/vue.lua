f36e77baf80cc75db0a1a10bffec7ade5f953d4f
2nthony <ant.hu@outlook.com>
2024-04-24 11:47:27 +0800

    fix(nvim): lsp, volar 2 with hybrid mode

--- .config/nvim/lua/plugins/extras/lang/vue.lua
+++ .config/nvim/lua/plugins/extras/lang/vue.lua
@@ -1,37 +1,35 @@
 -- vue
--- enable take over mode, disable tsserver
--- TODO: should improve for monorepo
+-- hybrid mode, https://github.com/vuejs/language-tools#hybrid-mode-configuration-requires-vuelanguage-server-version-200
+
 return {
   "nvim-lspconfig",
   opts = function(_, opts)
-    -- get path from current buffer
-    local current_path = vim.fn.expand("%:p:h")
-    local util = require("lspconfig.util")
-    local project_root = util.find_node_modules_ancestor(current_path)
+    local mason_registry = require("mason-registry")
+    local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+      .. "/node_modules/@vue/language-server"
 
-    local vue_path = util.path.join(project_root, "node_modules", "vue")
-    local is_vue = vim.fn.isdirectory(vue_path) == 1
-    if is_vue then
-      opts.servers.volar = {
-        filetypes = {
-          "vue",
-          "javascript",
-          "javascript.jsx",
-          "typescript",
-          "typescript.tsx",
-          "javascriptreact",
-          "typescriptreact",
-          "json",
+    opts.servers.volar = {}
+
+    opts.servers.tsserver = {
+      init_options = {
+        plugins = {
+          {
+            name = "@vue/typescript-plugin",
+            location = vue_language_server_path,
+            languages = { "vue" },
+            -- configNamespace = "typescript",
+            -- enableForWorkspaceTypeScriptVersions = true,
+          },
         },
-      }
-      opts.servers.tsserver = {
-        autostart = false,
-        root_dir = function()
-          return false
-        end,
-        single_file_support = false,
-      }
-    end
+      },
+      filetypes = {
+        "typescript",
+        "javascript",
+        "javascriptreact",
+        "typescriptreact",
+        "vue",
+      },
+    }
 
     return opts
   end,
