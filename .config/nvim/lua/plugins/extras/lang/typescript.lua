3fb1b9a12cee3e9e13ed8404b9a1cf6d25105659
2nthony <ant.hu@outlook.com>
2024-04-24 11:09:11 +0800

    Revert "feat(nvim): lsp, use typescript-tools.nvim for tsserver"
    This reverts commit 3b75b90ffbb42543541a180fdbe966b89f4b82e5.

--- .config/nvim/lua/plugins/extras/lang/typescript.lua
+++ .config/nvim/lua/plugins/extras/lang/typescript.lua
@@ -1,29 +1,20 @@
+-- typescript.nvim archived
+
 return {
   { import = "lazyvim.plugins.extras.lang.typescript" },
 
   {
     "nvim-lspconfig",
     opts = {
-      setup = {
-        -- return `true` don't setup with lspconfig
-        tsserver = function()
-          return true
-        end,
+      servers = {
+        tsserver = {
+          settings = {
+            completions = {
+              completeFunctionCalls = true,
+            },
+          },
+        },
       },
-    },
-  },
-
-  {
-    "pmizio/typescript-tools.nvim",
-    opts = {
-      settings = {
-        complete_function_calls = true,
-        expose_as_code_action = "all",
-      },
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "neovim/nvim-lspconfig",
     },
   },
 }
