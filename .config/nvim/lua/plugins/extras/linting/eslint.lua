7446ca93921bd0c9c557dee60b5faa7e83d19a39
2nthony <ant.hu@outlook.com>
2024-04-27 11:52:54 +0800

    feat(nvim): eslint, auto detech flat config file

--- .config/nvim/lua/plugins/extras/linting/eslint.lua
+++ .config/nvim/lua/plugins/extras/linting/eslint.lua
@@ -9,7 +9,6 @@ local settings = vscode.get_settings()
 local formatOnSave = settings["editor.formatOnSave"]
 local codeActionsOnSave = settings["editor.codeActionsOnSave"] or {}
 local eslintFixAllOnSave = codeActionsOnSave["source.fixAll.eslint"]
-local useFlatConfig = settings["eslint.experimental.useFlatConfig"]
 
 vim.g.autoformat = formatOnSave
 
@@ -18,34 +17,74 @@ return {
   {
     "neovim/nvim-lspconfig",
     lazy = true,
-    opts = {
-      servers = {
-        eslint = {
-          settings = {
-            experimental = {
-              useFlatConfig = useFlatConfig,
-            },
-          },
+    opts = function(_, opts)
+      local root = LazyVim.root.cwd()
+      local util = require("lspconfig.util")
+      local useFlatConfig = false
+
+      local flat_config_file_patterns = {
+        "eslint.config.mjs",
+        "eslint.config.cjs",
+        "eslint.config.js",
+      }
+      for _, file in ipairs(flat_config_file_patterns) do
+        local path = util.path.join(root, file)
+        local match = util.path.is_file(path)
+
+        if match then
+          useFlatConfig = true
+          break
+        end
+      end
+      useFlatConfig = useFlatConfig or settings["eslint.useFlatConfig"] or settings["eslint.experimental.useFlatConfig"]
+
+      opts.servers.eslint = {
+        settings = {
+          useFlatConfig = useFlatConfig,
         },
-      },
-      setup = {
-        eslint = function()
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            callback = function(event)
-              if eslintFixAllOnSave then
-                local client = vim.lsp.get_active_clients({ bufnr = event.buf, name = "eslint" })[1]
-                if client then
-                  local diag =
-                    vim.diagnostic.get(event.buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
-                  if #diag > 0 then
-                    vim.cmd("EslintFixAll")
-                  end
+      }
+      opts.setup.eslint = function()
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          callback = function(event)
+            if eslintFixAllOnSave then
+              local client = vim.lsp.get_active_clients({ bufnr = event.buf, name = "eslint" })[1]
+              if client then
+                local diag = vim.diagnostic.get(event.buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
+                if #diag > 0 then
+                  vim.cmd("EslintFixAll")
                 end
               end
-            end,
-          })
-        end,
-      },
-    },
+            end
+          end,
+        })
+      end
+    end,
+    -- opts = {
+    --   servers = {
+    --     eslint = {
+    --       settings = {
+    --         useFlatConfig = useFlatConfig,
+    --       },
+    --     },
+    --   },
+    --   setup = {
+    --     eslint = function()
+    --       vim.api.nvim_create_autocmd("BufWritePre", {
+    --         callback = function(event)
+    --           if eslintFixAllOnSave then
+    --             local client = vim.lsp.get_active_clients({ bufnr = event.buf, name = "eslint" })[1]
+    --             if client then
+    --               local diag =
+    --                 vim.diagnostic.get(event.buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
+    --               if #diag > 0 then
+    --                 vim.cmd("EslintFixAll")
+    --               end
+    --             end
+    --           end
+    --         end,
+    --       })
+    --     end,
+    --   },
+    -- },
   },
 }
