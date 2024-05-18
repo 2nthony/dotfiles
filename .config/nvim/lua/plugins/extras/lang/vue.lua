15fba41ab626aab71c0b6b658da1357af533130e
2nthony <ant.hu@outlook.com>
2024-05-18 18:33:36 +0800

    fix(nvim): vue, use lazyvim extra

--- .config/nvim/lua/plugins/extras/lang/vue.lua
+++ .config/nvim/lua/plugins/extras/lang/vue.lua
@@ -1,36 +1,3 @@
--- vue
--- hybrid mode, https://github.com/vuejs/language-tools#hybrid-mode-configuration-requires-vuelanguage-server-version-200
-
 return {
-  "nvim-lspconfig",
-  opts = function(_, opts)
-    local mason_registry = require("mason-registry")
-    local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
-      .. "/node_modules/@vue/language-server"
-
-    opts.servers.volar = {}
-
-    opts.servers.tsserver = {
-      init_options = {
-        plugins = {
-          {
-            name = "@vue/typescript-plugin",
-            location = vue_language_server_path,
-            languages = { "vue" },
-            -- configNamespace = "typescript",
-            -- enableForWorkspaceTypeScriptVersions = true,
-          },
-        },
-      },
-      filetypes = {
-        "typescript",
-        "javascript",
-        "javascriptreact",
-        "typescriptreact",
-        "vue",
-      },
-    }
-
-    return opts
-  end,
+  { import = "lazyvim.plugins.extras.lang.vue" },
 }
