dd1399dae91dfb98d6380fd75792621ca9c54c4c
2nthony <ant.hu@outlook.com>
2024-06-17 11:22:06 +0800

    chore(nvim): use octo extra

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,4 +1,6 @@
 return {
+  { import = "lazyvim.plugins.extras.util.octo" },
+
   {
     "NeogitOrg/neogit",
     cmd = { "Neogit", "NeogitResetState" },
@@ -44,19 +46,5 @@ return {
         },
       }
     end,
-  },
-
-  {
-    "pwntester/octo.nvim",
-    cmd = "Octo",
-    opts = {
-      default_to_projects_v2 = true,
-      default_merge_method = "squash",
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
   },
 }
