37973ff71c1ccf18c9f305765d344696d01affa2
2nthony <ant.hu@outlook.com>
2024-08-03 16:19:05 +0800

    fix(nvim): comment keybind conflicts

--- .config/nvim/lua/plugins/extras/coding/comment.lua
+++ .config/nvim/lua/plugins/extras/coding/comment.lua
@@ -3,28 +3,38 @@
 -- https://github.com/ayamir/nvimdots/blob/main/lua/modules/configs/editor/comment.lua
 
 return {
-  { "echasnovski/mini.comment", cond = vim.g.vscode },
-
-  {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-    lazy = true,
-    event = { "VeryLazy" },
-    init = function()
-      vim.g.skip_ts_context_commentstring_module = true
-    end,
-    opts = {},
-  },
-
   {
     "numToStr/Comment.nvim",
-    lazy = true,
     event = { "VeryLazy" },
-    opts = function(_, opts)
-      opts.ignore = "^$"
+    keys = {
+      {
+        "gb",
+        "<Plug>(comment_toggle_blockwise_visual)",
+        desc = "Toggle comment blockwise",
+        mode = { "x" },
+      },
+    },
+    opts = {
+      ignore = "^$",
+      mappings = {
+        basic = false,
+        extra = false,
+      },
+    },
+    config = function(_, opts)
       opts.pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+      require("Comment").setup(opts)
     end,
     dependencies = {
-      "JoosepAlviste/nvim-ts-context-commentstring",
+      {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+        lazy = true,
+        event = { "VeryLazy" },
+        init = function()
+          vim.g.skip_ts_context_commentstring_module = true
+        end,
+        opts = {},
+      },
     },
   },
 }
