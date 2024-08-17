10be995c7abc08806c0debc9f4217d24598878f9
2nthony <ant.hu@outlook.com>
2024-08-18 02:31:08 +0800

    fix(nvim): move vitesse colorscheme options

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -11,6 +11,8 @@ return {
       dim_nc = true,
       cmp_cmdline_disable_search_highlight_group = true,
       diagnostic_virtual_text_background = true,
+      -- transparent_background = false,
+      transparent_float_background = false,
     },
   },
   {
