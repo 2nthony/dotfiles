674894c79c2bf0161f0125055b16aaab7a40d848
2nthony <ant.hu@outlook.com>
2024-08-18 21:29:14 +0800

    chore(nvim): avante update deps

--- .config/nvim/lua/plugins/extras/ai/avante.lua
+++ .config/nvim/lua/plugins/extras/ai/avante.lua
@@ -9,13 +9,14 @@ return {
     build = "make",
     dependencies = {
       "nvim-tree/nvim-web-devicons",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
       {
         "grapp-dev/nui-components.nvim",
         dependencies = {
           "MunifTanjim/nui.nvim",
         },
       },
-      "nvim-lua/plenary.nvim",
       {
         "MeanderingProgrammer/render-markdown.nvim",
         opts = {
