015f3b3297c63aec0a83dae5d8446841acaa41aa
2nthony <ant.hu@outlook.com>
2024-10-21 16:36:01 +0800

    refactor(nvim): telescope project, extend layzvim preset

--- .config/nvim/lua/plugins/extras/util/project.lua
+++ .config/nvim/lua/plugins/extras/util/project.lua
@@ -1,60 +1,43 @@
 -- https://www.lazyvim.org/plugins/extras/util.project
--- https://github.com/coffebar/project.nvim
 
-local action = "Telescope projects"
-local cmd = ":" .. action .. "<cr>"
+local M = require("lazyvim.plugins.extras.util.project")
 
-return {
-  {
-    "telescope.nvim",
-    dependencies = {
-      "2nthony/project.nvim",
-      event = "VeryLazy",
-      keys = {
-        { "<leader>fp", cmd, desc = "Projects" },
-      },
-      opts = {
-        detection_methods = { "pattern" },
-        patterns = {
-          "!>home",
-          "!=tmp",
-          ".git",
-          "_darcs",
-          ".hg",
-          ".bzr",
-          ".svn",
-          "Makefile",
-          "=package.json", -- monorepo
-        },
-        exclude_dirs = {
-          "~/.local/*",
-          "~/.cache/*",
-          "~/.cargo/*",
-          "~/.node_modules/*",
-        },
-        on_changed = function()
-          local has = require("lazyvim.util").has
-
-          if has("persistence.nvim") then
-            require("persistence").load()
-          end
-        end,
-      },
-      config = function(_, opts)
-        require("project_nvim").setup(opts)
-        require("telescope").load_extension("projects")
-      end,
+local project = {
+  "2nthony/project.nvim",
+  opts = {
+    detection_methods = { "pattern" },
+    patterns = {
+      "!>home",
+      "!=tmp",
+      ".git",
+      "_darcs",
+      ".hg",
+      ".bzr",
+      ".svn",
+      "Makefile",
+      "=package.json", -- monorepo
     },
-  },
-  {
-    "dashboard-nvim",
-    opts = function(_, dashboard)
-      table.insert(dashboard.config.center, 4, {
-        action = action,
-        desc = " Projects",
-        icon = " ",
-        key = "p",
-      })
+    exclude_dirs = {
+      "~/.local/*",
+      "~/.cache/*",
+      "~/.cargo/*",
+      "~/.node_modules/*",
+    },
+    on_changed = function()
+      local has = require("lazyvim.util").has
+
+      if has("persistence.nvim") then
+        require("persistence").load()
+      end
     end,
   },
 }
+
+for _, spec in ipairs(M) do
+  if spec[1] == "ahmedkhalf/project.nvim" then
+    spec[1] = project[1]
+    spec.opts = project.opts
+  end
+end
+
+return M
