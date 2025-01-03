a4ab396254b968d7a692ddabb5d1c859cfca7be0
2nthony <ant.hu@outlook.com>
2025-01-03 11:41:28 +0800

    fix(nvim): refactoring debug console log

--- .config/nvim/lua/plugins/extras/editor/refactoring.lua
+++ .config/nvim/lua/plugins/extras/editor/refactoring.lua
@@ -1,25 +1,28 @@
 -- support print object
 
+local printf_statements_js = {
+  'console.log(" %s");',
+}
+local print_var_statements_js = {
+  'console.log(" %s ", %s);',
+}
+
 return {
   { import = "lazyvim.plugins.extras.editor.refactoring" },
   {
     "refactoring.nvim",
     opts = {
       printf_statements = {
-        javascript = {
-          'console.log(" %s");',
-        },
-        typescript = {
-          'console.log(" %s");',
-        },
+        javascript = printf_statements_js,
+        typescript = printf_statements_js,
+        javascriptreact = printf_statements_js,
+        typescriptreact = printf_statements_js,
       },
       print_var_statements = {
-        javascript = {
-          'console.log(" %s ", %s);',
-        },
-        typescript = {
-          'console.log(" %s ", %s);',
-        },
+        javascript = print_var_statements_js,
+        javascriptreact = print_var_statements_js,
+        typescript = print_var_statements_js,
+        typescriptreact = print_var_statements_js,
       },
     },
   },
