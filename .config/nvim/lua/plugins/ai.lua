df329bae55b10a48d70176c967f816775ebe3787
2nthony <ant.hu@outlook.com>
2026-01-29 21:33:41 +0800

    fix(nvim): ai, avante integrated

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -2,71 +2,22 @@ vim.g.ai_cmp = false
 
 return {
   { import = "lazyvim.plugins.extras.ai.codeium" },
-
-  {
-    "which-key.nvim",
-    opts = {
-      spec = {
-        { "<leader>a", group = "+ai" },
-      },
-    },
-  },
+  { import = "lazyvim.plugins.extras.ai.avante" },
 
   {
     "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
     opts = {
       mappings = {
         submit = {
           insert = "<C-CR>",
         },
       },
-    },
-    build = "make",
-    dependencies = {
-      "folke/snacks.nvim",
-      "stevearc/dressing.nvim",
-      "ibhagwan/fzf-lua",
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
-
-  {
-    "yetone/avante.nvim",
-    opts = {
       provider = "openai",
       providers = {
         openai = {
           model = "deepseek-chat",
           endpoint = "https://api.deepseek.com",
         },
-      },
-      windows = {
-        width = 40,
       },
     },
   },
